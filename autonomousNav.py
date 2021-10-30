5ba346bd6aebf1baf1495480fba98ad70d6a53f5
chethani76 <chethaniwijesekara@gmail.com>
2021-10-30 21:01:59 +0530

    initial script

---
+++ autonomousNav.py
@@ -0,0 +1,12 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+import argparse  
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default='127.0.0.1:14550')
+args = parser.parse_args()
+
+# Connect to the Vehicle
+print 'Connecting to vehicle on: %s' % args.connect
+vehicle = connect(args.connect, baud=921600, wait_ready=True)
